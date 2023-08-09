9af94796395061f37bf169c8b6f13bdf71ec1c88
Techharik <khariprasath30@gmail.com>
2023-08-09 15:16:46 +0530

    contact app with firesbase started

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="bg-green-800 text-2xl text-center text-slate-100 py-4">Conatct App</h1>
+    </>
+  )
+}
+
+export default App
